cc2f246c6bf35d488aad568669b7a96259d05e30
testsabirweb <testsabirweb@gmail.com>
2024-02-18 22:15:00 +0530

    temp

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module tutorial
+module github.com/testsabirweb/tutorial
 
 go 1.22.0
 
--- main.go
+++ main.go
@@ -1,46 +1,12 @@
 package main
 
 import (
-    "fmt"
-    "maps"
+	"fmt"
+
+	"github.com/google/uuid"
 )
 
+// go get github.com/google/uuid
 func main() {
-
-    m := make(map[string]int)
-
-    m["k1"] = 7
-    m["k2"] = 13
-
-    fmt.Println("map:", m)
-
-    v1 := m["k1"]
-    fmt.Println("v1:", v1)
-
-    v3 := m["k3"]
-    fmt.Println("v3:", v3)
-
-    fmt.Println("len:", len(m))
-
-    delete(m, "k2")
-    fmt.Println("map:", m)
-
-    clear(m)
-    fmt.Println("map:", m)
-
-    _, prs := m["k2"]
-    fmt.Println("prs:", prs)
-
-    n := map[string]int{"foo": 1, "bar": 2}
-    fmt.Println("map:", n)
-
-    n2 := map[string]int{"foo": 1, "bar": 2}
-    if maps.Equal(n, n2) {
-        fmt.Println("n == n2")
-    }
-
-    // Iteration on map
-    for k, v := range n2 { 
-        fmt.Printf("key[%s] value[%d]\n", k, v)
-    }
+	fmt.Println(uuid.New().String())
 }
